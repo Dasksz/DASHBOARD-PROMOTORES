c1a79cd77229226bf54c7be3b9df6d1e94a61463
Daniel César <144414027+Dasksz@users.noreply.github.com>
2026-03-11 09:50:34 -0300

    Update app.js

--- js/app/app.js
+++ js/app/app.js
@@ -20264,7 +20264,7 @@ const supervisorGroups = new Map();
     const RACK_OPTIONS = [
         "Mini Lego", "Lego 4C", "Lego 4C Smart", "4C Arramado", "5C", "6C",
         "7C - P", "7C - M", "7C - G", "8C", "Ilha Quadrada", "Giratório",
-        "Carrossel", "Rack de Amendoim", "Botadeiro", "Rack Toddynho", "Rack FOODS", "Nenhum"
+        "Carrossel", "Rack de Amendoim", "Botadeiro", "Gaiola", "Rack Toddynho", "Rack FOODS", "Nenhum"
     ];
 
     let selectedRackOptions = new Set();
