830eadb7b06654c5615c5452bcc0005e8fd3e903
Daniel César <144414027+Dasksz@users.noreply.github.com>
2026-03-13 17:11:18 -0300

    Delete debug_dates.js

--- debug_dates.js
+++
@@ -1,5 +0,0 @@
-const appFile = './js/worker.js';
-const fs = require('fs');
-let content = fs.readFileSync(appFile, 'utf8');
-
-console.log(content.match(/salesDataRaw.forEach\(rawRow => \{.*?dtPed.*?\}\)/s));
