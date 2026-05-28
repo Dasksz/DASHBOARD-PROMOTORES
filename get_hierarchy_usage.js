2ea035594618a1c73bc5399c24aed0de1c0d33c3
Daniel César <144414027+Dasksz@users.noreply.github.com>
2026-05-28 13:52:12 -0300

    Revert "⚡ Bolt: Prevent ColumnarDataset Proxy hydration memory leak"

--- get_hierarchy_usage.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let app = fs.readFileSync('js/app/app.js', 'utf8');
-let match = app.match(/function getHierarchyFilteredClients[\s\S]*?return result;\s*\}/);
-console.log(match[0]);
